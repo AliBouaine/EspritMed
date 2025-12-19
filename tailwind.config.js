8153d12bb46fbb5ce111176a5f6050cd5ae29c94
Nefzi_Firas <fnefzi466@gmail.com>
2025-12-19 21:42:51 +0100

    ajouter mon travail

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html", "./src/**/*.{js,ts,jsx,tsx,html}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#314584",
+        secondary: "#ff9346",
+        tertiary: "#6e757a",
+      },
+      fontFamily: {
+        body: ["Work Sans", "sans-serif"],
+      },
+      fontSize: {
+        navSize: "24px",
+        heroSize: "74px",
+        pSize: "18px",
+      },
+    },
+  },
+  plugins: [],
+};
